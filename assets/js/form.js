6f8713866b6c99987d36ac2b9118036e416454d5
B Water <brennw47@gmail.com>
2024-03-25 16:43:18 -0700

    Updated form.js
    Updated the form.js file to make it so that when the form is submitted the blog post data is stored to the localStorage.

--- assets/js/form.js
+++ assets/js/form.js
@@ -4,3 +4,20 @@ submitButton.addEventListener("click", function (event) {
   event.preventDefault();
   window.location.href = "blog.html";
 });
+
+const username = document.querySelector('#un');
+const title = document.querySelector('#t1');
+const content = document.querySelector('#c1');
+
+submitButton.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  const blogdata = {
+    Author: username.value,
+    Title: title.value,
+    Content: content.value,
+  };
+
+  localStorage.setItem('blogdata', JSON.stringify(blogdata));
+  renderContent();
+});
