0ea153e1d3ba05c1f5c52e0565cdff36a3b9af25
B Water <brennw47@gmail.com>
2024-03-26 14:10:40 -0700

    Update form/logic.js
    Tested out a few different function methods to parse the data retrieved in the local storage. Added a .trim to lines 16-18 to ensure no whitespacing causes any issues. Also changed blogdata to blogData in the form.js.

--- assets/js/form.js
+++ assets/js/form.js
@@ -10,14 +10,14 @@ const title = document.querySelector("#t1");
 const content = document.querySelector("#c1");
 submitButton.addEventListener("click", function (event) {
   event.preventDefault();
-  console.log(username.value, title.value, content.value, "data");
+  // console.log(username.value, title.value, content.value, "data");
   if (username.value && title.value && content.value) {
-    const blogdata = {
-      Author: username.value,
-      Title: title.value,
-      Content: content.value,
+    const blogData = {
+      Author: username.value.trim(),
+      Title: title.value.trim(),
+      Content: content.value.trim(),
     };
-    localStorage.setItem("blogdata", JSON.stringify(blogdata));
+    localStorage.setItem("blogData", JSON.stringify(blogData));
     renderContent();
   } else {
     alert("Please enter all fields");
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,9 @@
-const blogdata = JSON.parse(localStorage.getItem('blogdata'));
+const blogData = (0)
+// const blog = getBlogsFromLocalStorage
+function getBlogsFromLocalStorage() {
+    JSON.parse(localStorage.getItem('blogData'));
+}
+console.log(blogData)
+//fetch from local storage from array
+//push to populate the local storage on page load with event listener
+// blogData.getBlogsFromLocalStorage()
