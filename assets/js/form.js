e281381cbe932b2423370c9385a97f86e96ea366
B Water <brennw47@gmail.com>
2024-03-26 12:41:23 -0700

    Update form.js
    Updated the form.js to have an if statement that says if the username title and content values are all true with a string then the local storage will set an item else alert to enter all fields then go to the main content.

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,23 +1,28 @@
 const submitButton = document.querySelector("#submission");
 
+// submitButton.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   window.location.href = "blog.html";
+// });
+
+const username = document.querySelector("#un");
+const title = document.querySelector("#t1");
+const content = document.querySelector("#c1");
 submitButton.addEventListener("click", function (event) {
   event.preventDefault();
+  console.log(username.value, title.value, content.value, "data");
+  if (username.value && title.value && content.value) {
+    const blogdata = {
+      Author: username.value,
+      Title: title.value,
+      Content: content.value,
+    };
+    localStorage.setItem("blogdata", JSON.stringify(blogdata));
+    renderContent();
+  } else {
+    alert("Please enter all fields");
+  }
+});
+function renderContent() {
   window.location.href = "blog.html";
-});
-
-const username = document.querySelector('#un');
-const title = document.querySelector('#t1');
-const content = document.querySelector('#c1');
-
-submitButton.addEventListener('click', function (event) {
-  event.preventDefault();
-
-  const blogdata = {
-    Author: username.value,
-    Title: title.value,
-    Content: content.value,
-  };
-
-  localStorage.setItem('blogdata', JSON.stringify(blogdata));
-  renderContent();
-});
+}
