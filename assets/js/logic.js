f2b8e23575d46139542eeb623fa523487e560855
B Water <brennw47@gmail.com>
2024-03-27 13:02:12 -0700

    Update blog.html/logic.js
    Updated the blog.html have new ids for the section tags children. Updated the logic.js to have the element ids taken from the new blog.html ids, and made the for loop add in the content into the blog so its displayed in the main content.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,25 +1,34 @@
 let blogEntries = JSON.parse(localStorage.getItem('blogData'));
 function getBlogsFromLocalStorage() {
 
-    // document.getElementById('title-1').textContent = blogEntries[0].Title;
+    document.getElementById('title-1').textContent = blogEntries[0].Title;
     document.getElementById('content-1').textContent = blogEntries[0].Content;
     document.getElementById('author-1').textContent = `Author: ${blogEntries[0].Author}`;
 
-    // document.getElementById('title-2').textContent = blogEntries[1].Title;
+    document.getElementById('title-2').textContent = blogEntries[1].Title;
     document.getElementById('content-2').textContent = blogEntries[1].Content;
     document.getElementById('author-2').textContent = `Author: ${blogEntries[1].Author}`;
+
+    document.getElementById('title-3').textContent = blogEntries[2].Title;
+    document.getElementById('content-3').textContent = blogEntries[2].Content;
+    document.getElementById('author-3').textContent = `Author: ${blogEntries[2].Author}`;
+
+    document.getElementById('title-4').textContent = blogEntries[3].Title;
+    document.getElementById('content-4').textContent = blogEntries[3].Content;
+    document.getElementById('author-4').textContent = `Author: ${blogEntries[3].Author}`;
 }
 
 for (let i = 0; i < 4; i++) {
     const elements = blogEntries[i];
-    console.log(elements);
-    let titleEl=`title-${[i]}`
-    console.log(titleEl);
+    let titleEl=`title-${[i]}`;
+    // console.log(titleEl);
     document.getElementById(`title-${[i+1]}`).textContent = blogEntries[i].Title;
+    document.getElementById(`content-${[i+1]}`).textContent = blogEntries[i].Content;
+    document.getElementById(`author-${[i+1]}`).textContent = blogEntries[i].Author;
 }
 // template literal string
 //
-getBlogsFromLocalStorage();
+// getBlogsFromLocalStorage();
 // console.log(blogEntry);
 // const blog = getBlogsFromLocalStorage
 // function getBlogsFromLocalStorage() {
@@ -28,7 +37,3 @@ getBlogsFromLocalStorage();
 //fetch from local storage from array
 //push to populate the local storage on page load with event listener
 // blogData.getBlogsFromLocalStorage()
-
-function saveBlogEntry(blogEntry){
-    console.log('saveBlogEntry')
-}
