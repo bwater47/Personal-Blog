42307bce5729da491ce92aca5be8cae435eeae11
B Water <brennw47@gmail.com>
2024-03-26 21:13:15 -0700

    Update blog.html/logic.js files
    Updated the blog.html to have new h2 p and span id attribute names so they work with the javascript. Updated the logic.js to make the text content grab more elements from the html so that the elements that are grabbed are grabbed from local storage. Also made a template literal string for the title to test if it populates into the blog. Added some more comments to help out with the rest of the work.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,24 @@
-const blogEntries = JSON.parse(localStorage.getItem('blogData'));
+let blogEntries = JSON.parse(localStorage.getItem('blogData'));
 function getBlogsFromLocalStorage() {
-    document.getElementById('title1').textContent = blogEntries.Title;
-    document.getElementById('content1').textContent = blogEntry.Content;
-    document.getElementById('author1').textContent = `Author: ${blogEntry.Author}`;
+
+    // document.getElementById('title-1').textContent = blogEntries[0].Title;
+    document.getElementById('content-1').textContent = blogEntries[0].Content;
+    document.getElementById('author-1').textContent = `Author: ${blogEntries[0].Author}`;
+
+    // document.getElementById('title-2').textContent = blogEntries[1].Title;
+    document.getElementById('content-2').textContent = blogEntries[1].Content;
+    document.getElementById('author-2').textContent = `Author: ${blogEntries[1].Author}`;
 }
 
+for (let i = 0; i < 4; i++) {
+    const elements = blogEntries[i];
+    console.log(elements);
+    let titleEl=`title-${[i]}`
+    console.log(titleEl);
+    document.getElementById(`title-${[i+1]}`).textContent = blogEntries[i].Title;
+}
+// template literal string
+//
 getBlogsFromLocalStorage();
 // console.log(blogEntry);
 // const blog = getBlogsFromLocalStorage
